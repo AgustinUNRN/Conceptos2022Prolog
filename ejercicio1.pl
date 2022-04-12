40a4b5c3450a44d7ebd9fbca7f3e5590449840f9
AgustinUNRN <91021716+AgustinUNRN@users.noreply.github.com>
2022-04-12 12:13:30 -0300

    Update ejercicio1.pl
    avanzando con el ejercicio 1, falta entenderlo

--- ejercicio1.pl
+++ ejercicio1.pl
@@ -20,12 +20,29 @@ Represente los siguientes hechos:
 
 A continuación, escriba los objetivos en Prolog necesarios para responder a las preguntas indicadas a continuación:
     a. ¿Quiere Manuel a María?
+        quiere(manuel,maria). 
+        false.
     b. ¿Quiere Manuel a María y a Pedro?
+        quiere(manuel,maria,pedro). 
+        false.
     c. ¿Quiénes son los que quieren a María?
+        quiere(X,maria).
+        X = pedro ;
+        X = juan ;
+        X = belen.
     d. ¿Quiénes son los que se quieren mutuamente?
     e. ¿Quiénes son los que se quieren a sí mismos?
+        quiere(X,X). 
+        false.
     f. ¿Se quiere Manuel a sí mismo?
+        quiere(manuel,manuel).
+        false.
+•   Alguien quiere a todos los demás   .
+        quiere(alguien,pedro,maria,belen,juan). 
+        true.
     g. ¿Hay alguien que quiera a alguien?
+        quiere(X,alguien). 
+        false.
     h. ¿Hay alguien que quiera a todo el mundo?*/
 
 quiere(pedro,maria). %Pedro quiere a María.
@@ -41,7 +58,8 @@ quiere(maria,juan). %quiere(maria,juan).
 quiere(belen,juan). %quiere(belen,juan).
 quiere(belen,maria). %quiere(belen,maria).
 
-                    %Todos quieren a Juan y a María.
-                    %Alguien quiere a Juan y a Pedro.
-                    %Alguien quiere a María.
-                    %Alguien quiere a todos los demás.
+quiere(todos,juan,maria). %Todos quieren a Juan y a María.
+quiere(alguien,juan,pedro). %Alguien quiere a Juan y a Pedro.
+%quiere(alguien,maria). %Alguien quiere a María.
+quiere(alguien,pedro,maria,belen,juan). %Alguien quiere a todos los demás.
+
