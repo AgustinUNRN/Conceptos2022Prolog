a76342eaab9c7693ee48517757c89e01af4c34c0
AgustinUNRN <91021716+AgustinUNRN@users.noreply.github.com>
2022-05-11 11:33:43 -0300

    Create ejecicio4.pl
    eje 4 termina3

---
+++ ejecicio4.pl
@@ -0,0 +1,12 @@
+/*Ejercicio 4
+La siguiente es la nómina de personal de una empresa:
+Departamento de ventas: empleada María, cadetes Juan y Roque
+Departamento de compras: empleada Nora, cadete Pedro
+Departamento de administración: empleados Felipe y Hugo, cadeta Ana.
+Escribir un programa Prolog que modele a esta empresa, tal que puedan responderse las siguientes consultas:
+• ¿Quiénes trabajan en el departamento de compras? ¿y en el de ventas?
+• Dadas dos personas, ¿trabajan en el mismo departamento?
+• Dadas dos personas a y b, ¿puede a darle órdenes a b?
+• Decimos que a puede darle órdenes a b si y sólo si trabajan en el mismo departamento y a tiene un cargo
+superior a b. Se considera que “empleado” es un cargo superior a “cadete”.
+*/
