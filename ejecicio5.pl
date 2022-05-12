e069865cb842f35e8d533276c73b808c13bcc30d
AgustinUNRN <91021716+AgustinUNRN@users.noreply.github.com>
2022-05-12 12:23:51 -0300

    Termina3 el eje 5

--- ejecicio5.pl
+++ ejecicio5.pl
@@ -8,6 +8,7 @@ Incluir en el programa la siguiente información:
 El programa debe ser capaz de responder, p.ej., a la siguiente consulta:
 ¿cuáles son los rivales de Argentina?
 otorgando como respuestas "Nigeria", "Japón" y "Escocia".}*/
+
 seleccion(colombia,grupoA).
 seleccion(camerun,grupoA).
 seleccion(jamaica,grupoA).
@@ -17,14 +18,10 @@ seleccion(nigeria,grupoB).
 seleccion(japon,grupoB).
 seleccion(escocia,grupoB).
 
+rivales(Selec,Salida):- seleccion(Selec,Grupo), seleccion(Salida,Grupo),Salida\=Selec. 
 
-rivales(A):- seleccion(A,G), seleccion(S,G),S\=A. 
-
-%seleccion(argentina,G), seleccion(S,G),S\=argentina. 
-
-%esTierno(Persona):- persona(Persona), forall(leGusta(Persona, Alimento), esDulce(Alimento)).
-
-
-%esTierno(Persona)
-
+%	el primer llamado a seleccion obtiene el grupo de la seleccion ingresada.
+%	el segundo seleccion obtiene todas las selecciones del grupo correspondiente.
+%	el ultimo termino evita mostrar la seleccion ingresada por parametro.
+%	para el llamado rivales(argentina,Rival).
 
